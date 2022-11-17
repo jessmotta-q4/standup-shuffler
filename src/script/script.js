a457aa27de14b89d3b9351d680bc0596333b3f18
Jess Motta <jessica.limamotta@q4inc.com>
2022-11-17 15:32:21 -0300

    add job description

--- src/script/script.js
+++ src/script/script.js
@@ -3,27 +3,27 @@ let counter = 0;
 
 var speakers = [
   [
-    { name: "Julia Joachimowicz", img: "/src/img/Julia.png" },
-    { name: "Emma Mallon", img: "/src/img/Emma.png" },
-    { name: "Jen Grajkowski", img: "/src/img/Jen_Grajkowski.png" },
-    { name: "Jennifer Guo", img: "/src/img/Jen_Guo.png" },
-    { name: "Luis Díaz", img: "/src/img/Luis.png" },
-    { name: "Julianna Hindemith", img: "/src/img/Julianna.png" },
-    { name: "Eduardo Arrangóiz", img: "/src/img/Eduardo.png" },
+    { name: "Julia Joachimowicz", img: "/src/img/Julia.png", job: "Implementation Coordinator" },
+    { name: "Emma Mallon", img: "/src/img/Emma.png", job: "Implementation Coordinator"},
+    { name: "Jen Grajkowski", img: "/src/img/Jen_Grajkowski.png", job: "Implementation Manager" },
+    { name: "Jennifer Guo", img: "/src/img/Jen_Guo.png", job: "Implementation Manager" },
+    { name: "Luis Díaz", img: "/src/img/Luis.png", job: "Implementation Coordinator" },
+    { name: "Julianna Hindemith", img: "/src/img/Julianna.png", job: "Implementation Manager" },
+    { name: "Eduardo Arrangóiz", img: "/src/img/Eduardo.png", job: "Implementation Coordinator" },
   ],
   [
-    { name: "Abner Rojas", img: "/src/img/Abner.png" },
-    { name: "Augusto Pavía Rosas", img: "/src/img/Augusto.png" },
-    { name: "Cesar Villegas", img: "/src/img/Cesar.png" },
-    { name: "Michael Li", img: "/src/img/Michael.png" },
-    { name: "Jéssica Motta", img: "/src/img/Jessica.png" },
-    { name: "Kathryn Robertson", img: "/src/img/Kathryn.png" },
-    { name: "Marcos Herrera", img: "/src/img/Marcos.png" },
-    { name: "Shaun Seeram", img: "/src/img/Shaun.png" },
-    { name: "Enrique Lozano", img: "/src/img/Enrique.png" },
-    { name: "Filip Swierczynski", img: "/src/img/Filip.png" },
-    { name: "João Dantas", img: "/src/img/Joao.png" },
-    { name: "Robert Ling", img: "/src/img/Robert.png" },
+    { name: "Abner Rojas", img: "/src/img/Abner.png", job: "Web Developer III" },
+    { name: "Augusto Pavía Rosas", img: "/src/img/Augusto.png", job: "Quality Assurance Analyst" },
+    { name: "Cesar Villegas", img: "/src/img/Cesar.png", job: "Web Developer I" },
+    { name: "Michael Li", img: "/src/img/Michael.png", job: "Web Developer I" },
+    { name: "Jéssica Motta", img: "/src/img/Jessica.png", job: "Web Developer I" },
+    { name: "Kathryn Robertson", img: "/src/img/Kathryn.png", job: "Data Entry Specialist" },
+    { name: "Marcos Herrera", img: "/src/img/Marcos.png", job: "Web Developer II" },
+    { name: "Shaun Seeram", img: "/src/img/Shaun.png", job: "Web Developer I" },
+    { name: "Enrique Lozano", img: "/src/img/Enrique.png", job: "Data Entry Specialist" },
+    { name: "Filip Swierczynski", img: "/src/img/Filip.png", job: "Web Developer II" },
+    { name: "João Dantas", img: "/src/img/Joao.png", job: "Web Developer II" },
+    { name: "Robert Ling", img: "/src/img/Robert.png", job: "Quality Assurance Analyst"},
   ],
 ];
 
@@ -44,14 +44,17 @@ let shuffler = () => {
   
   if (counter <= 6) {
     document.getElementById("speakerName").innerHTML = speaker.name;
+    document.getElementById("speakerJob").innerHTML = speaker.job;
     document.getElementById("speakerImage").setAttribute("src", speaker.img);
   } else {
     group = shuffle(speakers[1]);
     if (typeof speaker !== "undefined") {
       document.getElementById("speakerName").innerHTML = speaker.name;
+      document.getElementById("speakerJob").innerHTML = speaker.job;
       document.getElementById("speakerImage").setAttribute("src", speaker.img);
     } else {
       document.getElementById("speakerName").innerHTML = "End of list";
+      document.getElementById("speakerJob").innerHTML = "";
       document
         .getElementById("speakerImage")
         .setAttribute("src", "/src/img/finished_list.jpg");
